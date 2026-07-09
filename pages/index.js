f70b12b7daefbd7f54799832a5dec309f860328f
Rubens Mariano Lindner <79090925+rubens-mariano@users.noreply.github.com>
2026-07-09 15:17:12 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mumu, eu amo você. Se você me ama, dá uma risadinha e me dá um cheiro ❤️</h1>
+    return <h1>Mumu, eu amo você. Se você me ama, dá uma risadinha e me dá um cheiro ❤️😎</h1>
 }
 
 export default Home;
